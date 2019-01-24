4bdc616e8531b9b807cc2fe5f76cf119b23d0cbe
Stephen Synchronicity <sholsen@alumni.emory.edu>
2019-01-24 18:15:25 -0500

    Simplify Manual

--- R/General.R
+++ R/General.R
@@ -90,10 +90,11 @@ Mode <- function(v) {
 #' @param dat \code{(data.frame, matrix)} data.frame, vector or matrix object
 #' @return \item{list} object with the following values:
 #' \describe{
-#'  \item{`NA`}{ A vector
+#'  \item{`NA`}{ A vector}
 #'    \itemize{
-#'      \item{\code{No.}}{ The Number of NA}
-#'      \item{\code{P}}{ The percentage as a decimal of \eqn{\frac{\text{the number of NA values}}{\text{the total number of values in the column}}}.}}}
+#'      \item{\code{No.}}{The Number of NA}
+#'      \item{\code{P}}{ The percentage as a decimal of \eqn{\frac{\text{the number of NA values}}{\text{the total number of values in the column}}}.}
+#'    }
 #'  \item{\code{Indices}}{ The indices of the row numbers that contain NA in that column, useful for locating NA and examining adjacent data.}}
 #' @examples
 #' findna(c(1, NA, 2))
